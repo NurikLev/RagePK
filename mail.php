b1082155c53c5683dbc19493a57ac953433b82d0
NurikLev <147980128+NurikLev@users.noreply.github.com>
2023-10-17 16:47:25 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,38 +1,48 @@
-<?php 
+<?php
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
+//Import PHPMailer classes into the global namespace//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 $mail->CharSet = 'utf-8';
 
 $email = $_POST['user_email'];
+    
+    try {
+ //Server settings$mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output$mail->isSMTP(); //Send using SMTP$mail->Host = 'smtp.example.com'; //Set the SMTP server to send through
+    $mail->SMTPAuth = true; //Enable SMTP authentication
+    $mail->Username = 'nurik.sait.rage@mail.ru'; //SMTP username
+    $mail->Password = 'Nurmagomed05'; //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+    $mail->Port 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+    //Recipients
+    $mail->setFrom('nurik.sait.rage@mail.ru', 'Mailer');
+ $mail->addAddress('ebanashka.gorade@inbox.ru', 'Joe User'); //Add a recipient
+ //    $mail->addAddress('ellen@example.com'); //Name is optional
+ //    $mail->addReplyTo('info@example.com', 'Information');
+ // $mail->addCC('cc@example.com');
+ // $mail->addBCC('bcc@example.com');
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'nurik.sait.rage@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Nurmagomed05'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+ //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz'); //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
 
-$mail->setFrom('nurik.sait.rage@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('ebanashka.gorade@inbox.ru');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+    //Content
+    $mail->isHTML(true); //Set email format to HTML
+    $mail->Subject = 'Заявка с тестового сайта';
+ $mail->Body = '' .$email . ' оставил заявку';
+ $mail->AltBody = '';
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$email . ' оставил заявку';
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-	header('location: index.html');
+ $mail->send();
+ echo 'Сообщение отправлено';
+} catch (Exception $e) {
+ echo "Error: {$mail->ErrorInfo}";
 }
 ?>
